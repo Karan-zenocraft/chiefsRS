fb4485c82ff04b30bcfe966bd59ea5494978316f
root <root@zsoahm127.zenosys.local>
2019-05-15 19:57:40 +0530

    common controller

---
+++ common/components/CommonController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace common\components;
+
+use Yii;
+use yii\web\Controller;
+use yii\filters\VerbFilter;
+
+/**
+ * Controller is the customized base controller class.
+ * All controller classes for this application should extend from this base class.
+ */
+class CommonController extends Controller
+{
+
+    public function init()
+    {
+        parent::init();
+    }
+
+}
