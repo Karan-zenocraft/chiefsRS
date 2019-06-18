151689ed278684ed42a890e25248950a79853a1c
root <root@zsoahm127.zenosys.local>
2019-06-18 18:46:04 +0530

    backend config file changed

--- backend/config/main.php
+++ backend/config/main.php
@@ -99,7 +99,7 @@ return [
             ]
         ],
         'request' => [
-            'baseUrl'=>($_SERVER['HTTP_HOST'] == "localhost") ? '/chiefsRS/admin' : '/admin',
+             'baseUrl'=>($_SERVER['HTTP_HOST'] == "localhost") ? '/chiefsRS/admin' : '/admin',
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => 'pbB0NvlmxlWRk7XFCN_7XUC2uvX0vyCD',
         ],
