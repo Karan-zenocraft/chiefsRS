9b42f8742b362e96cce345e425b88644e97e9024
root <root@zsoahm127.zenosys.local>
2019-06-18 19:28:48 +0530

    revert htaccess and common config changes

--- common/config/main.php
+++ common/config/main.php
@@ -24,14 +24,6 @@ if ($_SERVER['HTTP_HOST'] == "localhost"){
             ],
         ],
     ],
-      'urlManager' => [
-            'enablePrettyUrl' => true,
-            'showScriptName' => false,
-            'scriptUrl'=> ($_SERVER['HTTP_HOST'] == "localhost") ? '/chiefsRS/common' : '/common',
-            ],
-        'request' => [
-             'baseUrl'=>($_SERVER['HTTP_HOST'] == "localhost") ? '/chiefsRS/common' : '/common',
-        ],
    /*     'mail' => [
             'class' => 'yii\swiftmailer\Mailer',
             //'viewPath' => '@common/mail',
@@ -94,14 +86,6 @@ if ($_SERVER['HTTP_HOST'] == "localhost"){
             ],
         ],
     ],
-    'urlManager' => [
-            'enablePrettyUrl' => true,
-            'showScriptName' => false,
-            'scriptUrl'=> ($_SERVER['HTTP_HOST'] == "localhost") ? '/chiefsRS/common' : '/common',
-            ],
-        'request' => [
-             'baseUrl'=>($_SERVER['HTTP_HOST'] == "localhost") ? '/chiefsRS/common' : '/common',
-        ],
    /*     'mail' => [
             'class' => 'yii\swiftmailer\Mailer',
             //'viewPath' => '@common/mail',
