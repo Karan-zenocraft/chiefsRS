90857d8a58b1370e3bd2939821effc3ba11a5abc
Rutusha Joshi <rutusha@zsoahm127.zenosys.local>
2019-07-03 19:53:30 +0530

    remote database user changed

--- common/config/main.php
+++ common/config/main.php
@@ -62,7 +62,7 @@ if ($_SERVER['HTTP_HOST'] == "localhost"){
         ],
     ],
 ];
-}else if(($_SERVER['HTTP_HOST'] == "localhost:8012")){
+}else if(($_SERVER['HTTP_HOST'] == "localhost:8012") || ($_SERVER['HTTP_HOST'] == "121.55.237.213:8012")){
            return [
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
     'components' => [
@@ -72,67 +72,8 @@ if ($_SERVER['HTTP_HOST'] == "localhost"){
         'db' => [
             'class' => 'yii\db\Connection',
             'dsn'      => 'mysql:host=localhost;dbname=chiefsRS',
-            'username' => 'root',
-            'password' => '',
-            'charset' => 'utf8',
-        ],
-         'assetManager' => [
-        'bundles' => [
-            'kartik\form\ActiveFormAsset' => [
-                'bsDependencyEnabled' => false // do not load bootstrap assets for a specific asset bundle
-            ],
-        ],
-    ],
-   /*     'mail' => [
-            'class' => 'yii\swiftmailer\Mailer',
-            //'viewPath' => '@common/mail',
-            // send all mails to a file by default. You have to set
-            // 'useFileTransport' to false and configure a transport
-            // for the mailer to send real emails.
-            'useFileTransport' => false,
-            //'useFileTransport' => false,//to send mails to real email addresses else will get stored in your mail/runtime folder
-            //comment the following array to send mail using php's mail function
-            'transport' => [
-                'class' => 'Swift_SmtpTransport',
-                'host' => 'smtp.gmail.com',
-                'username' => 'inx.email001@gmail.com',
-                'password' => 'mail@001',
-                'port' => '587',
-                'encryption' => 'tls',
-            ],
-        ],*/
-        'mail' => [
-            'class' => 'yii\swiftmailer\Mailer',
-            'viewPath' => '@common/mail',
-            // send all mails to a file by default. You have to set
-            // 'useFileTransport' to false and configure a transport
-            // for the mailer to send real emails.
-            'useFileTransport' => false,
-            //'useFileTransport' => false,//to send mails to real email addresses else will get stored in your mail/runtime folder
-            //comment the following array to send mail using php's mail function
-            'transport' => [
-                'class' => 'Swift_SmtpTransport',
-               'host' => 'smtp.gmail.com',
-               'username' => 'chefsrs123@gmail.com',
-               'password' => 'Admin@123',
-               'port' => '587',
-               'encryption' => 'tls',
-            ],
-        ],
-    ],
-];
-}else if(($_SERVER['HTTP_HOST'] == "121.55.237.213:8012")){
-           return [
-    'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
-    'components' => [
-        'cache' => [
-            'class' => 'yii\caching\FileCache',
-        ],
-        'db' => [
-            'class' => 'yii\db\Connection',
-            'dsn'      => 'mysql:host=121.55.237.213:8012;dbname=chiefsRS',
-            'username' => 'root',
-            'password' => '',
+            'username' => 'chiefsrsUser',
+            'password' => 'chiefsrs',
             'charset' => 'utf8',
         ],
          'assetManager' => [
