ad1db5804fffe8e127fe45240f256a76f2d07471
root <root@zsoahm127.zenosys.local>
2019-07-05 18:01:49 +0530

    add guest controller

---
+++ api/controllers/GuestController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace api\controllers;
+
+use Yii;
+use yii\helpers\Json;
+use common\components\Common;
+use yii\web\Controller;
+use api\components\ApiCoreController;
+use yii\web\NotFoundHttpException;
+use yii\filters\VerbFilter;
+use \yii\web\UploadedFile;
+use yii\db\Query;
+
+/* USE COMMON MODELS */
+use common\models\Users;
+use yii\helpers\ArrayHelper;
+use yii\imagine\Image;
+use common\models\DeviceDetails;
+use backend\controllers\SiteController;
+use common\models\UserActivities;
+use common\models\EmailFormat;
+use common\models\Reservations;
+/**
+ * MainController implements the CRUD actions for APIs.
+ */
+class GuestController extends \yii\base\Controller
+{
+        /*
+     * Function :
+     * Description : Add Guest
+     * Request Params :'user_id','auth_token'
+     * Response Params :
+     * Author :Rutusha Joshi
+     */
+       public function actionAddGuest(){
+      
+        //Get all request parameter
+        $amData = Common::checkRequestType();
+        $amResponse = $amReponseParam = [];
+
+        // Check required validation for request parameter.
+        $amRequiredParams = array( 'user_id' );
+        $amParamsResult   = Common::checkRequestParameterKey( $amData['request_param'], $amRequiredParams );
+
+        // If any getting error in request paramter then set error message.
+        if ( !empty( $amParamsResult['error'] ) ) {
+            $amResponse = Common::errorResponse( $amParamsResult['error'] );
+            Common::encodeResponseJSON( $amResponse );
+        }
+
+        $requestParam     = $amData['request_param'];
+        //Check User Status//
+        $this->matchUserStatus( $requestParam['user_id'] );
+        //VERIFY AUTH TOKEN
+        $authToken = Common::get_header( 'auth_token' );
+        Common::checkAuthentication( $authToken );
+        $snUserId = $requestParam['user_id'];
+        $model = Users::findOne( ["id" => $snUserId] );
+        if ( !empty( $model ) ) {
+            $restaurant_id = !empty($model->restaurant_id) ? $model->restaurant_id : "";
+            if(!empty($restaurant_id)){
+                $layouts = RestaurantLayout::find()->where(['restaurant_id'=>$restaurant_id,'status'=>Yii::$app->params['user_status_value']['active']])->asArray()->all();
+                if(!empty($layouts)){
+                    $ssMessage                                = 'User Floors Details.';
+
+                    $amReponseParam['Floors']             = $layouts;
+
+                    $amResponse = Common::successResponse( $ssMessage, $amReponseParam );
+                }else{
+                    $ssMessage  = 'There is no any floors added to your restaurant';
+                    $amResponse = Common::errorResponse( $ssMessage );
+                }
+            }else{
+                 $ssMessage  = 'You have not assigned any restaurant yet.';
+                 $amResponse = Common::errorResponse( $ssMessage );
+            }
+        }else {
+            $ssMessage  = 'Invalid User.';
+            $amResponse = Common::errorResponse( $ssMessage );
+        }
+        // FOR ENCODE RESPONSE INTO JSON //
+        Common::encodeResponseJSON( $amResponse );
+    }
+
+
+
+}
